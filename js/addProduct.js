fe0d55abfe07003cb1e15a91aab59fe0e5f0d420
jonathan401 <jonathangomina5049@gmail.com>
2022-09-01 21:09:13 +0100

    rename select field

--- js/addProduct.js
+++ js/addProduct.js
@@ -1,5 +1,3 @@
-console.log("welcome to the about product page :)");
-
 const productForm = document.querySelector("#add-product-form");
 const typeSwitch = document.querySelector("#productType");
 const typeForm = document.querySelector("#type-forms");
@@ -12,7 +10,7 @@ document.onreadystatechange = () => {
 
 // determine form to show function
 const showForm = () => {
-  const formIds = ["Disc", "Book", "Furniture"];
+  const formIds = ["DVD", "Book", "Furniture"];
   let filteredForms = formIds.filter((formId) => formId !== typeSwitch.value);
   filteredForms.map((form) =>
     typeForm.querySelector(`#${form}`).classList.add("hidden")
@@ -26,30 +24,28 @@ typeSwitch.addEventListener("change", () => {
   showForm();
 });
 
-// form markup
-let discHtml = `
-  <div class="fieldbox" id="Disc-switch">
-    <label>Size (mb)</label>
-    <input type="number" placeholder="Enter Disc Size" name="disc" required />
-  </div>`;
+// // form markup
+// let DVDHtml = `
+//   <div class="fieldbox" id="Disc-switch">
+//     <label>Size (mb)</label>
+//     <input type="number" placeholder="Enter Disc Size" name="disc" required />
+//   </div>`;
 
-let bookHtml = `
-  <div class="fieldbox" id="Book-switch">
-    <label>Weight (kg)</label>
-    <input type="number" placeholder="Enter Book Weight" name="weight" required />
-  </div>
-`;
+// let bookHtml = `
+//   <div class="fieldbox" id="Book-switch">
+//     <label>Weight (kg)</label>
+//     <input type="number" placeholder="Enter Book Weight" name="weight" required />
+//   </div>
+// `;
 
-let furnitureHtml = `
-  <div class="fieldbox" id="Furniture-switch">
-    <label>Dimension (cm)</label>
-    <input type="number" placeholder="Enter Furniture Width" name="width" required / >
-    <input type="number" placeholder="Enter Furniture Height" name="height"required / >
-    <input type="number" placeholder="Enter Furniture length" name="length" required / >
-  </div>
-`;
-
-console.log(typeForm);
+// let furnitureHtml = `
+//   <div class="fieldbox" id="Furniture-switch">
+//     <label>Dimension (cm)</label>
+//     <input type="number" placeholder="Enter Furniture Width" name="width" required / >
+//     <input type="number" placeholder="Enter Furniture Height" name="height"required / >
+//     <input type="number" placeholder="Enter Furniture length" name="length" required / >
+//   </div>
+// `;
 
 // const showFormType = (typeValue) => {
 //   switch (typeValue) {
@@ -111,25 +107,27 @@ const redirectUser = (endpoint) => {
     .splice(0, currentLocation.length - 1)
     .join("/");
   let redirectUrl = `${protocol}//${host}${path}/${endpoint}`;
-  let t =
-    "http://127.0.0.1:5500/Scandiweb-Project-main/Scandiweb%20Test%20Assignment/Productpage.html";
-  console.log(redirectUrl, "\n", redirectPage);
-
   location.assign(redirectUrl);
 };
 
+// productForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const selectFieldValue = typeSwitch.value;
+//   // const formData = {
+//   //   productId: productForm.sku.value.trim(),
+//   //   productName: productForm.name.value.trim(),
+//   //   productPrice: `$${productForm.price.value.trim()}`,
+//   //   // [formatPropName(`${typeSwitch.value}`)]: getFieldValue(typeSwitch.value),
+//   // };
+//   // console.log(formData, "id");
+//   console.log("submitted");
+
+//   // addProduct(formData);
+//   // productForm.reset();
+//   // setTimeout(() => redirectUser("Productpage.html"), 100);
+// });
 productForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const selectFieldValue = typeSwitch.value;
-  const formData = {
-    productId: productForm.sku.value.trim(),
-    productName: productForm.name.value.trim(),
-    productPrice: `$${productForm.price.value.trim()}`,
-    // [formatPropName(`${typeSwitch.value}`)]: getFieldValue(typeSwitch.value),
-  };
-  console.log(formData, "id");
-
-  // addProduct(formData);
-  // productForm.reset();
-  // setTimeout(() => redirectUser("Productpage.html"), 100);
+  console.log("submitted");
 });
+console.log(productForm);
